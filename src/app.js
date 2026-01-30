beb682a27186338cd740d681959622cd099e00b5
Ramón Camacho Rojas <wenreidesign@gmail.com>
2026-01-30 10:44:36 +0000

    Generador de dominios terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+let brand = ["wenrei", "ramoncamacho"];
+let rol = ["a11y", "studio", "lab", "design", "UX", "UI"];
+let extensions = ["com", "net", "us", "io", "es"]
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < brand.length; i++) {
+  for (let j = 0; j < rol.length; j++) {
+    let fullName = brand[i] + rol[j];
+
+      for (let e = 0; e < extensions.length; e++) {
+        let ext = extensions[e];
+
+        if (fullName.endsWith(ext)){
+          let hackName = fullName.slice (0, -ext.length);
+          console.log(`${hackName}.${ext} <-- domain hacks`);
+        }
+
+        else{
+          console.log(`${fullName}.${ext}`);
+        }
+      }
+    }
+  }
